7b9fab924e57975a335d1aa2110cc8f3463a3ebb
DucDuyVu <vdduy0102@gmail.com>
2026-07-07 21:55:38 +0700

    fix

--- src/main/java/com/javaweb/repository/UsersRepository.java
+++ src/main/java/com/javaweb/repository/UsersRepository.java
@@ -1,10 +1,12 @@
 package com.javaweb.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.javaweb.entity.UsersEntity;
 
 public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
-	
+	Optional<UsersEntity> findByEmail(String email);
 }
