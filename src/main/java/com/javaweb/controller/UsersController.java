f84866f24196981839ef51c7bed9fbebafe0ea98
DucDuyVu <vdduy0102@gmail.com>
2026-07-07 11:10:06 +0700

    done update profile

--- src/main/java/com/javaweb/controller/UsersController.java
+++ src/main/java/com/javaweb/controller/UsersController.java
@@ -1,11 +1,16 @@
 package com.javaweb.controller;
 
+import java.awt.color.ProfileDataException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.javaweb.dto.request.UpdateProfileRequest;
 import com.javaweb.dto.response.ProfileResponse;
 import com.javaweb.entity.UsersEntity;
 import com.javaweb.service.UsersService;
@@ -20,5 +25,14 @@ public class UsersController {
 	public ProfileResponse profile(Authentication authentication) {
 		UsersEntity user = (UsersEntity)authentication.getPrincipal();
 		return usersService.getProfile(user);
+	}
+	
+	@PutMapping("/profile")
+	public ProfileResponse updateProfile(Authentication authentication,
+			@RequestBody UpdateProfileRequest updateProfileRequest) {
+		
+		// Lấy thông tin người dùng đang đăng nhập
+		UsersEntity user = (UsersEntity) authentication.getPrincipal();
+		return usersService.updateProfile(user, updateProfileRequest);
 	}
 }
--- src/main/java/com/javaweb/service/impl/UsersServiceImpl.java
+++ src/main/java/com/javaweb/service/impl/UsersServiceImpl.java
@@ -1,17 +1,23 @@
 package com.javaweb.service.impl;
 
-import org.springframework.security.core.Authentication;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.javaweb.dto.response.LoginResponse;
+import com.javaweb.dto.request.UpdateProfileRequest;
 import com.javaweb.dto.response.ProfileResponse;
 import com.javaweb.entity.UsersEntity;
+import com.javaweb.repository.UsersRepository;
 import com.javaweb.service.UsersService;
 
 
 @Service
 public class UsersServiceImpl implements UsersService{
 
+	@Autowired
+	UsersRepository usersRepository;
+	
 	@Override
 	public ProfileResponse getProfile(UsersEntity user) {
 		ProfileResponse profileResponse = new ProfileResponse();
@@ -21,5 +27,26 @@ public class UsersServiceImpl implements UsersService{
 		profileResponse.setUserId(user.getId());
 		profileResponse.setRole(user.getRole().name());
 		return profileResponse;
-	}	
+	}
+
+	@Override
+	public ProfileResponse updateProfile(UsersEntity user, UpdateProfileRequest updateProfileRequest) {
+		
+		user.setFullName(updateProfileRequest.getFullName());
+		user.setPhone(updateProfileRequest.getPhone());
+		user.setAvatarURL(updateProfileRequest.getAvatarUrl());
+		user.setUpdatedAt(LocalDateTime.now());
+		
+		UsersEntity updateUser = usersRepository.save(user); // save thông tin update
+		
+		ProfileResponse profileResponse = new ProfileResponse();
+		profileResponse.setFullName(updateUser.getFullName());
+		profileResponse.setPhone(updateUser.getPhone());
+		profileResponse.setUserId(updateUser.getId());
+		profileResponse.setEmail(updateUser.getEmail());
+		profileResponse.setRole(updateUser.getRole().name());
+		return profileResponse; // trả về client
+	}
+
+		
 }
